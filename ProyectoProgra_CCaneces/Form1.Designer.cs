a202dc660b61be176ef52243cc066274decca591
Luis <Luis@DESKTOP-BG0HS4R>
2025-05-13 22:04:57 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoProgra_CCaneces/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ProyectoProgra_CCaneces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxConversion = new System.Windows.Forms.ComboBox();
+            this.textBoxCantidad = new System.Windows.Forms.TextBox();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxConversion
+            // 
+            this.comboBoxConversion.FormattingEnabled = true;
+            this.comboBoxConversion.Location = new System.Drawing.Point(93, 54);
+            this.comboBoxConversion.Name = "comboBoxConversion";
+            this.comboBoxConversion.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxConversion.TabIndex = 0;
+            // 
+            // textBoxCantidad
+            // 
+            this.textBoxCantidad.Location = new System.Drawing.Point(267, 55);
+            this.textBoxCantidad.Name = "textBoxCantidad";
+            this.textBoxCantidad.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCantidad.TabIndex = 1;
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(148, 172);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(0, 13);
+            this.labelResultado.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.textBoxCantidad);
+            this.Controls.Add(this.comboBoxConversion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxConversion;
+        private System.Windows.Forms.TextBox textBoxCantidad;
+        private System.Windows.Forms.Label labelResultado;
+    }
+}
+
---
+++ ProyectoProgra_CCaneces/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoProgra_CCaneces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxConversion.Items.Add("Kilómetros a Millas");
+            comboBoxConversion.Items.Add("Millas a Kilómetros");
+            comboBoxConversion.Items.Add("Metros a Pies");
+            comboBoxConversion.Items.Add("Pies a Metros");
+            comboBoxConversion.Items.Add("Centímetros a Pulgadas");
+            comboBoxConversion.Items.Add("Pulgadas a Centímetros");
+            comboBoxConversion.SelectedIndex = 0;
+        }
+
+        private void buttonConvertir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double cantidad = double.Parse(textBoxCantidad.Text);
+                double resultado = 0;
+                string unidadFinal = "";
+
+                switch (comboBoxConversion.SelectedIndex)
+                {
+                    case 0:
+                        resultado = cantidad * 0.621371;
+                        unidadFinal = "mi";
+                        break;
+                    case 1:
+                        resultado = cantidad * 1.60934;
+                        unidadFinal = "km";
+                        break;
+                    case 2:
+                        resultado = cantidad * 3.28084;
+                        unidadFinal = "ft";
+                        break;
+                    case 3:
+                        resultado = cantidad / 3.28084;
+                        unidadFinal = "m";
+                        break;
+                    case 4:
+                        resultado = cantidad / 2.54;
+                        unidadFinal = "in";
+                        break;
+                    case 5:
+                        resultado = cantidad * 2.54;
+                        unidadFinal = "cm";
+                        break;
+                }
+
+                labelResultado.Text = $"Resultado: {resultado:F2} {unidadFinal}";
+            }
+            catch
+            {
+                MessageBox.Show("Por favor ingrese una cantidad válida.");
+            }
+        }
+
+    }
+}
+
